52d17272ee1b77d7c2dc63fddcab0d2e23a78494
Danielandro <mtme13@hotmail.com>
2019-08-29 02:11:10 +0100

    test: Numbers not divisible by 3, 5 or both

---
+++ FizzBuzzTests.cs
@@ -0,0 +1,74 @@
+using Xunit;
+using fizzBuzz;
+
+public class TestClass
+{
+
+  // numbers divisible by 3
+  [Theory]
+  [InlineData(3)]
+  [InlineData(6)]
+  [InlineData(9)]
+
+  public void NumbersDivisibleByThreeTheory(int num)
+  {
+    Assert.True(FizzBuzz.DivisibleByThree(num));
+  }
+
+  // numbers divisible by 5
+  [Theory]
+  [InlineData(5)]
+  [InlineData(10)]
+  [InlineData(20)]
+
+  public void NumbersDivisibleByFiveTheory(int num)
+  {
+    Assert.True(FizzBuzz.DivisibleByFive(num));
+  }
+
+  // numbers divisible by 3 & 5
+  [Theory]
+  [InlineData(15)]
+  [InlineData(30)]
+  [InlineData(45)]
+
+  public void NumbersDivisibleByThreeAndFiveTheory(int num)
+  {
+    Assert.True(FizzBuzz.DivisibleByThreeAndFive(num));
+  }
+
+  // numbers NOT divisible by 3
+  [Theory]
+  [InlineData(2)]
+  [InlineData(4)]
+  [InlineData(5)]
+
+  public void NumbersNotDivisibleByThreeTheory(int num)
+  {
+    Assert.False(FizzBuzz.DivisibleByThree(num));
+  }
+
+  // numbers NOT divisible by 5
+  [Theory]
+  [InlineData(3)]
+  [InlineData(6)]
+  [InlineData(8)]
+
+  public void NumbersNotDivisibleByFiveTheory(int num)
+  {
+    Assert.False(FizzBuzz.DivisibleByFive(num));
+  }
+
+  // numbers NOT divisible by 3 & 5
+  [Theory]
+  [InlineData(3)]
+  [InlineData(5)]
+  [InlineData(16)]
+
+  public void NumbersNotDivisibleByThreeAndFiveTheory(int num)
+  {
+    Assert.False(FizzBuzz.DivisibleByThreeAndFive(num));
+  }
+
+
+}
